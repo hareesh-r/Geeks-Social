5361b8b96572d16cfdf799aa338d774d0c2ce5e8
harshi-2801 <80154912+harshi-2801@users.noreply.github.com>
2021-06-18 17:44:23 +0530

    Add files via upload

---
+++ minimum-viable-product/Components/bottomNavbar.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Dashboard from './dashboard';
+import LoginScreen from './loginScreen';
+
---
+++ minimum-viable-product/Components/dashboard.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Dashboard extends Component {
+    state = {  }
+    render() {
+        return (
+            <View>
+                <Text>This is Home Screen Component</Text>
+            </View>
+         );
+    }
+}
+
+export default Dashboard;
